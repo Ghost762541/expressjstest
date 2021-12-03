ff11b664fb50af43341e86898870a0602c02d992
ghost762541 <nastush96@mail.ru>
2021-12-03 10:26:02 +0700

    delete

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
+//const { Button } = require("bootstrap");
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 // загрузка файла через  require
 const fileData = require('./test.json')
-// настройка параметров шаблонизатора
+const schema = new mongoose.Schema({ title: 'string', accountId: 'number' });
+const Account = mongoose.model('Account', schema);
+
+
+mongoose.connect('mongodb://192.168.241.84:27017/test');
+
+
+//настройка параметров шаблонизатора
 app.set('view engine', 'pug');
 
-app.get('/', (req,res) => {
-    // рендер просит на вход первым параметром название шаблон-страницы без расширения
-    // вторым объект данных
-   res.render('index',{
-       product: fileData
+// button.onclick = function() {
+//     alert('Клик!');
+//   };
+
+app.get('/', async (req,res) => {
+    const first = new Account({title: "first", accountId: 1});
+    //await first.save();
+    
+    const accounts = await Account.find()
+    //console.log(accounts)
+    res.render('index', {
+        Account: first,
+        accounts,
    })
 })
 
-app.listen(8000)
+app.get('/new', async (req, res) => {
+    const first = new Account({title: "first", accountId: 1});
+    await first.save();
+    const accounts = await Account.find()
+    res.redirect('back');
+})
+
+app.get('/delete/:id', async (req, res) => {
+    Account.find({id: req.params.id}).remove().exec();
+    res.redirect('back');
+})
+
+app.listen(3000)
