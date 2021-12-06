db8962129339a90afff5959fbc3126b27887d042
ghost762541 <nastush96@mail.ru>
2021-12-06 13:29:43 +0700

    UserRouts

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
-//const { Button } = require("bootstrap");
 const express = require('express')
 const mongoose = require('mongoose')
+const UserRoute = require('./UserRoutes.js')
 const app = express()
 
-// загрузка файла через  require
-const fileData = require('./test.json')
 const schema = new mongoose.Schema({ title: 'string', accountId: 'number' });
 const Account = mongoose.model('Account', schema);
 
-
 mongoose.connect('mongodb://192.168.241.84:27017/test');
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
-//настройка параметров шаблонизатора
 app.set('view engine', 'pug');
+
+  
+app.use('/users', UserRoute);
 
 app.get('/', async (req,res) => {
     const first = new Account({title: "first", accountId: 1});
-    //await first.save();
     
     const accounts = await Account.find()
-    console.log(accounts)
     res.render('index', {
         Account: first,
         accounts,
@@ -28,23 +25,11 @@ app.get('/', async (req,res) => {
 })
 
 app.get('/new', async (req, res) => {
-    // const first = new Account({title: "first", accountId: 1});
-    // await first.save();
-    //const accounts = await Account.find()
-    res.render('form')
-    //res.redirect('/');
-
+    res.render('form');
 })
 
 app.post('/new', async (req, res) => {
     console.log(req.body.title);
-    // Account.count({}, function(err, result) {
-    //     if (err) {
-    //       console.log(err);
-    //     } else {
-    //         const first = new Account({title: req.body.title, accountId: result + 1});;
-    //     }
-    //   });
     Account.count({}, function( err, count){
         console.log( count );
     })
@@ -53,7 +38,6 @@ app.post('/new', async (req, res) => {
     const first = new Account({title: req.body.title, accountId: c});
     await first.save();
     res.redirect('/');
-    //res.send("ok");
 })
 
 app.get('/delete/:_id', async (req, res) => {
