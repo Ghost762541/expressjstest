a88f5c7a5a3a704f8d30636b12720aa7f5b0a35e
ghost762541 <nastush96@mail.ru>
2021-12-15 16:53:14 +0700

    photo

--- AccountRoutes.js
+++ AccountRoutes.js
@@ -5,10 +5,26 @@ const { User, Account } = require('./model.js');
 const moment = require('moment');
 const { nanoid } = require('nanoid');
 const md5 = require('md5');
+const fileUpload = require('../lib/index');
+const app = express();
+const bodyParser = require('body-parser');
+
+const uploadPath = '/home/user/tmp/files/';
+
+//app.use(express.static(__dirname + '/users/upload.pug'));
+AccountRoute.use(express.static(uploadPath));
+
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
 
 AccountRoute.use(function timeLog(req, res, next) {
     next();
 });
+
+AccountRoute.use(fileUpload());
 
 AccountRoute.get('/', async (req, res) => {
   const accounts = await Account.find();
@@ -86,6 +102,40 @@ AccountRoute.get('/:accountId/password/:_id', async (req, res) => {
   let passwordHash = md5(password);
   await User.updateOne({_id: req.params._id} , { $set: { passwordHash: passwordHash } }).exec();
   res.redirect('/accounts/' + accountId + '/users');
+})
+
+AccountRoute.get('/:accountId/profile/:_id/', async (req, res) => {
+  let user = await User.findOne({_id: req.params._id})
+  //console.log(user)
+  //let accountId = req.params.accountId;
+  //res.redirect('/accounts/' + accountId + '/users');
+  res.render('users/profile', user);
+})
+
+AccountRoute.get('/:accountId/profile/:_id/upload', async (req, res) => {
+  //res.redirect('/accounts/' + accountId + '/users');
+  let accountId = req.params.accountId;
+  let userId = req.params._id;
+  res.render('users/upload', {accountId, userId}) ;
+})
+
+AccountRoute.post('/:accountId/profile/:_id/upload', async (req, res) => {
+  let accountId = req.params.accountId;
+  let userId = req.params._id;
+  //let accountId = req.params.accountId;
+  //res.redirect('/accounts/' + accountId + '/users');
+  // if (!req.files || Object.keys(req.files).length === 0) {
+  //   return res.status(400).send('No files were uploaded.');
+  // }
+  await console.log(req.files);
+  let imgpath = req.params._id + ".png";
+  req.files.img.mv(uploadPath + imgpath )
+  
+  await User.updateOne({_id: req.params._id}, { $set: { imgpath: imgpath } }).exec();
+  
+  //res.json(imgpath);
+  //console.log(_dirname);
+  res.redirect('/accounts/' + accountId + '/profile/' + userId + '/')
 })
 
 module.exports = AccountRoute;
